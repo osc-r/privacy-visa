d0daa283798e1289a01eb3eef23bd58d562eb27a
oscar <osx@Oscars-MacBook-Pro.local>
2024-11-17 07:23:38 +0700

    add: connect with web3auth

--- src/components/forms/Login.form.tsx
+++ src/components/forms/Login.form.tsx
@@ -1,11 +1,25 @@
 import { Button, Grid2 as Grid } from "@mui/material";
 import { useRouter } from "next/navigation";
+import { web3auth } from "../../app/layout";
 
 export default function LoginForm() {
   const router = useRouter();
 
   const onClickConnectWeb3 = () => {
     router.replace("/cards");
+  };
+
+  const onClickConnectWeb3Auth = async () => {
+    // IMP START - Login
+    await web3auth.connect();
+    // IMP END - Login
+    if (web3auth.connected) {
+      router.replace("/cards");
+    }
+    /**
+     * in order to save a provider object
+     * might consider save it with zustand
+     *  */
   };
 
   return (
@@ -15,7 +29,7 @@ export default function LoginForm() {
           fullWidth
           variant="outlined"
           color="primary"
-          onClick={onClickConnectWeb3}
+          onClick={onClickConnectWeb3Auth}
         >
           Connect with Web3Auth
         </Button>
